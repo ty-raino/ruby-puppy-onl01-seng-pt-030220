871535602142e84355fd7d81edfca5966340bb63
Ty Raino <mstymetris@yahoo.com>
2020-03-23 00:25:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   @@all = []
 
   def initialize(name)
-    @@all << self
+    @@save = save
   end
 
   def self.all
@@ -12,5 +12,16 @@ class Dog
   def self.print_all
     @@all.each do |names|
       puts "#{names}"
+    end
+
+    def save 
+      @@all << self
+    end
+
+    def self.clear_all
+      @@all.clear
+    end
+
+    def self.class_variable_get(name_of_class_variable)
     end
   end
